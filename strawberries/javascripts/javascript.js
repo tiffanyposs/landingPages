4a2db5565640bd0fa49224a8ac97be62500a9694
tiffanyposs <tiffanyposs@gmail.com>
2016-02-27 17:49:26 -0500

    arrow and dots

--- strawberries/javascripts/javascript.js
+++ strawberries/javascripts/javascript.js
@@ -14,4 +14,12 @@ $( document ).ready(function(){
   	$('#become-strawberry').show();
   })
 
+
+  // scroll down arrows
+  $('.arrows a').on('click', function(){
+  	var location = $('.latest-work').offset().top - 20;
+  	// var height = $( window ).height();
+  	$('html, body').animate({ scrollTop: location }, 1000);
+  })
+
 });
